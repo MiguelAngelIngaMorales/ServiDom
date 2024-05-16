3770174241e88bd2ef1226330a06e67eb2033564
eddynmanuel <43767859+eddynmanuel@users.noreply.github.com>
2024-05-16 01:49:26 -0500

    Create c_login.js

---
+++ Controlador/c_login.js
@@ -0,0 +1,5 @@
+/*Script para redirigir al usuario a la página de catalogo*/
+function redirigirCatalogo() {
+    // Redirigir al usuario a la página del catálogo
+    window.location.href = 'V_catalogo.html';
+}
