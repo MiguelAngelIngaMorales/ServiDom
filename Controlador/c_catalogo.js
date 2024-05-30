cda3f3379ada3b3937255d9864fec1341742abff
MiguelAngelIngaMorales <152421813+MiguelAngelIngaMorales@users.noreply.github.com>
2024-05-30 08:11:58 -0500

    Delete Controlador directory

--- Controlador/c_catalogo.js
+++
@@ -1,4 +0,0 @@
-/*Script para redirigir al usuario a la página de registro*/
-function redirigirRegistro() {
-    window.location.href = 'V_Registro.html';
-}
--- Controlador/c_login.js
+++
@@ -1,5 +0,0 @@
-/*Script para redirigir al usuario a la página de catalogo*/
-function redirigirCatalogo() {
-    // Redirigir al usuario a la página del catálogo
-    window.location.href = 'V_catalogo.html';
-}
