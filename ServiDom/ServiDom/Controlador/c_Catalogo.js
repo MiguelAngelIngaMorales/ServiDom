4a10e410bbae12473e86cf1999fe07a751870b35
MiguelAngelIngaMorales <152421813+MiguelAngelIngaMorales@users.noreply.github.com>
2024-06-19 20:17:57 -0500

    Delete ServiDom/ServiDom directory

--- ServiDom/ServiDom/Controlador/c_Catalogo.js
+++
@@ -1,7 +0,0 @@
-/*Script para redirigir al usuario a la página de registro*/
-function redirigirRegistro() {
-    window.location.href = 'V_Registro.html';
-}
-function abrirPagina(url) {
-    window.location.href = url;
-}
--- ServiDom/ServiDom/Controlador/c_Registrar.js
+++
@@ -1,7 +0,0 @@
-function redirigirRegistroTrabajador() {
-    window.location.href = 'V_RegistroTrabajador.html';
-}
-
-function redirigirRegistroCliente() {
-    window.location.href = 'V_RegistroCliente.html';
-}
